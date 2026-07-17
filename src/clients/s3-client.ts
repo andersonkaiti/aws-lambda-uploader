724c048609fee370cccf467b56ae2e9c34f4357e
Anderson Kaiti <anderkaiti@gmail.com>
2026-07-17 02:31:16 -0300

    ✨ feat(s3): set bucket region explicitly

--- src/clients/s3-client.ts
+++ src/clients/s3-client.ts
@@ -1,3 +1,6 @@
 import { S3Client } from '@aws-sdk/client-s3'
+import { env } from '../config/env.ts'
 
-export const s3Client = new S3Client()
+export const s3Client = new S3Client({
+  region: env.BUCKET_REGION,
+})
--- src/config/env.ts
+++ src/config/env.ts
@@ -1,3 +1,4 @@
 export const env = {
   BUCKET_NAME: process.env.BUCKET_NAME,
+  BUCKET_REGION: process.env.BUCKET_REGION,
 }
